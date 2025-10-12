fe74645133f3937c6bf38d4677c915ff7279c27f
Brandon Kapp <b.kapp1010@gmail.com>
2025-10-12 10:22:11 -0700

    Add error property to NextAuth Session type

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,4 +1,4 @@
-// Extend NextAuth types to include user ID
+// Extend NextAuth types to include user ID and error
 import NextAuth, { DefaultSession } from "next-auth"
 
 declare module "next-auth" {
@@ -6,6 +6,7 @@ declare module "next-auth" {
     user: {
       id: string
     } & DefaultSession["user"]
+    error?: string
   }
 }
 
